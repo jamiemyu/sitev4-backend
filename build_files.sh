b6c2782d88a9a44f77a435bf5e0713900c816a66
Jamie <jamieminyu@gmail.com>
2025-08-26 13:56:25 -0700

    Fix build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,6 +6,10 @@ python3 -m pip install -U pip
 echo "Installing project dependencies..."
 python3 -m pip install -r requirements.txt
 
+echo "Migrating Database..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
 # Collect static files
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
