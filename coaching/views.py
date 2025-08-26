4dac309d193eff3d744c0c2962cfdcbef851d849
Jamie <jamieminyu@gmail.com>
2025-08-26 14:21:33 -0700

    Add main page response

--- coaching/views.py
+++ coaching/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the jamie's coaching site.")
+    return HttpResponse("Hello, world. You're at jamie's coaching site.")
