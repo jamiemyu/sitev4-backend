b66d73c19ddada72ee1ed51ce8d057cdf19ec43c
Jamie <jamieminyu@gmail.com>
2025-08-26 13:48:27 -0700

    Comment out build_files to fix error

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
-pip install -r requirements.txt
-python3.12.11 manage.py collectstatic --no-input --clear
+# pip install -r requirements.txt
+# python3.12.11 manage.py collectstatic --no-input --clear
