8fcf521aafa4931749bfe8d385545869b3b3035a
Jamie <jamieminyu@gmail.com>
2025-08-26 14:01:42 -0700

    Add static directory for collecting static files and comment out databases configuration

--- build_files.sh
+++ build_files.sh
@@ -6,10 +6,6 @@ python3 -m pip install -U pip
 echo "Installing project dependencies..."
 python3 -m pip install -r requirements.txt
 
-echo "Migrating Database..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
 # Collect static files
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
--- mysite/settings.py
+++ mysite/settings.py
@@ -79,10 +79,11 @@ WSGI_APPLICATION = "mysite.wsgi.app"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # TODO: Migrate to PostgreSQL
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
@@ -121,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
