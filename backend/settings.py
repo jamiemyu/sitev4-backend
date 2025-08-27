7b9aa7e85b7f31c8f4768e59ebf959ac7105e4ee
Jamie <jamieminyu@gmail.com>
2025-08-26 17:30:21 -0700

    Tries different static files definition

--- backend/settings.py
+++ backend/settings.py
@@ -128,8 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+# STATIC_URL = "/static/"
+# STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
