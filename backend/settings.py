3bea6dda2b166740f9d9bf3f7f8ff55b3bc6a437
Jamie <jamieminyu@gmail.com>
2025-08-27 12:54:15 -0700

    Delete print statements

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print("BASE_DIR = " + str(BASE_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -132,7 +131,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-print("STATICFILES_DIRS = " + str(STATICFILES_DIRS))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
