93a66c01eeba0a286b6bf2ba0df84ae9baadf092
Jamie <jamieminyu@gmail.com>
2025-08-27 12:55:23 -0700

    Add backend subdirectory to staticfiles path

--- backend/settings.py
+++ backend/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "backend" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
