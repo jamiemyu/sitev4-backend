1cc29f4385baea153157daf87241bf93ea897964
Jamie <jamieminyu@gmail.com>
2025-08-28 16:29:49 -0700

    Adds supabase DB environment variables

--- settings.py
+++ settings.py
@@ -9,14 +9,20 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os 
+
+import dotenv
+import os
 
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
+
+# Find and load the .env file.
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -29,7 +35,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",  # Local server
-    ".vercel.app", # Vercal domains e.g., sitev4.vercel.app
+    ".vercel.app",  # Vercal domains e.g., sitev4.vercel.app
 ]
 
 
@@ -52,10 +58,10 @@ INSTALLED_APPS = [
     "testimonials.apps.TestimonialsConfig",
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
