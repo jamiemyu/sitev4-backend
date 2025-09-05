898196a7a914ad3f3b230153cb34d89d94ffbafd
Jamie <jamieminyu@gmail.com>
2025-09-05 13:31:07 -0700

    Adds more documentation and cleans up unused imports

--- core/api/views.py
+++ core/api/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpRequest
 from django.http import HttpResponse
 
 
-def index(request):
+def index(_: HttpRequest) -> HttpResponse:
+    """
+    Returns a simple message when the user visits the site hosting the sitev4-backend API.
+    """
     return HttpResponse("You're at jamie's site.")
